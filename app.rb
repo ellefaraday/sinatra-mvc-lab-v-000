4dc4eee5b24da6f5341b6cde43932b18a8dddf23
Elle Faraday <faraday.elle@gmail.com>
2018-01-27 21:20:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/' do
+    @text = PigLatinizer.new(params[:text])
+    erb :pig_latin
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,25 @@
+require "pry"
+class PigLatinizer
+
+  def piglatinize(word)
+    word_arr = word.split(//)
+
+    if !word_arr[0].scan(/[AEIOUaeiou]/).empty?
+      word_arr << "way"
+    else
+      i = 0
+      while word_arr[i].scan(/[AEIOUaeiou]/).empty?
+        word_arr << word_arr.shift
+        i += 1
+      end
+      word_arr << "ay"
+    end
+    word_arr.join
+  end
+
+  #text_arr = text.split(" ")
+  #text_arr.collect do |word|
+  #end.join(" ")
+end
+
+PigLatinizer.new.piglatinize("hot")
