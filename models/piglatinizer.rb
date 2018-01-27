7c9d6b23e8ef629cc0fa0a510ff6b33e3d956282
Elle Faraday <faraday.elle@gmail.com>
2018-01-27 22:00:20 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,29 +1,14 @@
 require "pry"
 class PigLatinizer
 
-  #def piglatinize(word)
-  #  word_arr = word.split(//)
-  #  vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
-  #  if vowels.include?(word_arr[0])
-  #    word_arr << "way"
-  #  else
-  #    i = 0
-  #    while vowels.include?(word_arr[i]) == false || i == word_arr.length do
-  #      word_arr << word_arr.shift
-  #      i += 1
-  #    end
-  #    word_arr << "ay"
-  #  end
-  #  word_arr.join
-  #end
-
   def piglatinize(word)
     word_arr = word.split(//)
-    if !word_arr[0].scan(/[AEIOUaeiou]/).empty?
+    vowels = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"]
+    if vowels.include?(word_arr[0])
       word_arr << "way"
     else
       i = 0
-      while word_arr[i].scan(/[AEIOUaeiou]/).empty? do
+      while vowels.include?(word_arr[i]) == false || i != word_arr.length do
         word_arr << word_arr.shift
         i += 1
       end
@@ -31,6 +16,21 @@ class PigLatinizer
     end
     word_arr.join
   end
+
+  #def piglatinize(word)
+  #  word_arr = word.split(//)
+  #  if !word_arr[0].scan(/[AEIOUaeiou]/).empty?
+  #    word_arr << "way"
+  #  else
+  #    i = 0
+  #    while word_arr[i].scan(/[AEIOUaeiou]/).empty? do
+  #      word_arr << word_arr.shift
+  #      i += 1
+  #    end
+  #    word_arr << "ay"
+  #  end
+  #  word_arr.join
+  #end
 
   #text_arr = text.split(" ")
   #text_arr.collect do |word|
